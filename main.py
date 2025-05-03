05e7b52428aa33ec0846ca403d07f35311e6e546
dserver <ppdavisito89@gmail.com>
2025-05-03 00:43:17 -0400

    Project Init

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods = ['POST', 'GET'])
+def upload_file():
+    uploaded_filename = None
+    if request.method == 'POST':
+        file = request.files.get('image')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            uploaded_filename = filename
+            print(uploaded_filename)
+    return render_template('index.html', filename=uploaded_filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return redirect(url_for('static', filename=f'uploads/{filename}'))
 
 
 if __name__ == "__main__":
