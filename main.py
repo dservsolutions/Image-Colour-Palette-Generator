f62f6b1fbcac6b7fcc440790d32117aacb5fa08d
dserver <ppdavisito89@gmail.com>
2025-05-03 17:59:01 -0400

    Image loaded and saved successfully

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template
 import os
+import colorgram
+from PIL import Image
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
@@ -7,6 +9,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+file_uploaded = {}
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
@@ -23,7 +26,34 @@ def upload_file():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(path)
             print(f"Saved to {path}")
+            proper = get_image_properties(path)
     return render_template('index.html', filename=filename)
+
+# After processing the image, tells to the user what are the top 10 most common colors.
+# Color - color code - percentage
+def get_image_properties(image_path, num_colors=10):
+    try :
+        img = Image.open(image_path)
+        properties = {
+            "format" : img.format,
+            "size": img.size,
+            "mode": img.mode
+        }
+        colors = colorgram.extract(image_path, num_colors)
+        top_colors = []
+        for color in colors:
+            # color.rgb is a named tuple with r, g, b attributes
+            rgb_tuple = (color.rgb.r, color.rgb.g, color.rgb.b)
+            top_colors.append(rgb_tuple)
+            print(top_colors)
+            return top_colors
+        print(properties.values())
+        return properties
+
+    except FileNotFoundError:
+        print(f"Error: Image not found at {image_path}")
+        return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
