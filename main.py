36fccf2540b24d9e88df3e3dcf9a92096622905f
dserver <ppdavisito89@gmail.com>
2025-05-04 16:18:42 -0400

    Trying to get the image colors.

--- main.py
+++ main.py
@@ -27,36 +27,12 @@ def upload_file():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(path)
             print(f"Saved to {path}")
-            top10 = get_image_properties(path)
-            if top10:
-                color_name = webcolors.rgb_to_name(top10)
-                print(color_name)
 
     return render_template('index.html', filename=filename)
 
 # After processing the image, tells to the user what are the top 10 most common colors.
 # Color - color code - percentage
-def get_image_properties(image_path, num_colors=10):
-    try :
-        img = Image.open(image_path)
-        properties = {
-            "format" : img.format,
-            "size": img.size,
-            "mode": img.mode
-        }
-        colors = colorgram.extract(image_path, num_colors)
-        top_colors = []
-        for color in colors:
-            # color.rgb is a named tuple with r, g, b attributes
-            rgb_tuple = (color.rgb.r, color.rgb.g, color.rgb.b)
-            # top_colors.append(rgb_tuple)
-            top_colors.append(rgb_tuple)
-            return top_colors
-        return properties
-    except FileNotFoundError:
-        print(f"Error: Image not found at {image_path}")
-        return None
-
-
+def get_image_colors(image_path, num_colors=10):
+    pass
 if __name__ == '__main__':
     app.run(debug=True)
